239ed5142fb31c05650ba18b4744debb64946adb
Lam Mai <lam7.2521@gmail.com>
2022-01-19 18:11:52 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const gameEngine = new GameEngine();
+
+const ASSET_MANAGER = new AssetManager();
+
+ASSET_MANAGER.downloadAll(() => {
+  const canvas = document.getElementById('gameWorld');
+  const ctx = canvas.getContext('2d');
+  ctx.imageSmoothingEnabled = false;
+
+  gameEngine.init(ctx);
+
+  gameEngine.start();
+});
