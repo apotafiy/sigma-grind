98611004b259341b4404cba3f3cd027fb8ccb64f
Vlad Tregubov <treguv@uw.edu>
2022-01-20 23:28:50 -0800

    slow but y tho

--- main.js
+++ main.js
@@ -16,7 +16,9 @@ ASSET_MANAGER.downloadAll(() => {
   const ctx = canvas.getContext('2d');
   ctx.imageSmoothingEnabled = false;
   let gravity = 0.2;
-  gameEngine.addEntity(new Ground(gameEngine,1,0, 450, 18,4));
+  gameEngine.addEntity(new Ground(gameEngine,1,0, 450, 16,4));
+  gameEngine.addEntity(new Ground(gameEngine,0,640, 386,5 ,1));
+
   gameEngine.addEntity(new Ground(gameEngine,0,0, 0, 1,8));
   gameEngine.addEntity(new Ground(gameEngine,0,950, 0, 1,8));
   gameEngine.addEntity(new Mettaur(gameEngine, 800,0, gravity));
