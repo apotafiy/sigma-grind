401446dcc7ea6674a4295db10c7de4dbfa7607f5
Lam Mai <lam7.2521@gmail.com>
2022-01-20 23:33:02 -0800

    Add Mettaur assets

--- animator.js
+++ animator.js
@@ -1,54 +1,54 @@
 class Animator {
-    constructor(
-        spritesheet,
-        xStart,
-        yStart,
-        width,
-        height,
-        frameCount,
-        frameDuration
-    ) {
-        Object.assign(this, {
-            spritesheet,
-            xStart,
-            yStart,
-            width,
-            height,
-            frameCount,
-            frameDuration,
-        });
+  constructor(
+    spritesheet,
+    xStart,
+    yStart,
+    width,
+    height,
+    frameCount,
+    frameDuration
+  ) {
+    Object.assign(this, {
+      spritesheet,
+      xStart,
+      yStart,
+      width,
+      height,
+      frameCount,
+      frameDuration,
+    });
 
-        this.elapsedTime = 0;
-        this.totalTime = frameCount * frameDuration;
+    this.elapsedTime = 0;
+    this.totalTime = frameCount * frameDuration;
+  }
+
+  drawFrame(tick, ctx, x, y, scale) {
+    this.elapsedTime += tick;
+
+    if (this.elapsedTime > this.totalTime) {
+      this.elapsedTime -= this.totalTime;
     }
 
-    drawFrame(tick, ctx, x, y) {
-        this.elapsedTime += tick;
+    const frame = this.currentFrame();
 
-        if (this.elapsedTime > this.totalTime) {
-            this.elapsedTime -= this.totalTime;
-        }
+    ctx.drawImage(
+      this.spritesheet,
+      this.xStart - this.width * frame,
+      this.yStart,
+      this.width,
+      this.height,
+      x,
+      y,
+      this.width * scale,
+      this.height * scale
+    );
+  }
 
-        const frame = this.currentFrame();
+  currentFrame() {
+    return Math.floor(this.elapsedTime / this.frameDuration);
+  }
 
-        ctx.drawImage(
-            this.spritesheet,
-            this.xStart - this.width * frame,
-            this.yStart,
-            this.width,
-            this.height,
-            x,
-            y,
-            this.width,
-            this.height
-        );
-    }
-
-    currentFrame() {
-        return Math.floor(this.elapsedTime / this.frameDuration);
-    }
-
-    isDone() {
-        return this.elapsedTime >= this.totalTime;
-    }
+  isDone() {
+    return this.elapsedTime >= this.totalTime;
+  }
 }
