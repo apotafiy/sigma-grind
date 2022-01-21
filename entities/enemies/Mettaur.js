201c4294d73a709a79f3db43c383fef6382e08ee
Vlad Tregubov <treguv@uw.edu>
2022-01-20 20:49:35 -0800

    added background and basic ground building tool

--- entities/enemies/Mettaur.js
+++ entities/enemies/Mettaur.js
@@ -1,12 +1,5 @@
 class Mettaur{
-    /**
-     *  draw method params
-     * start x position
-     * start y position
-     * sprite width
-     * sprite height
-     * frame count
-     * 
+    /** 
      * time per frame
      */
     /**
@@ -14,11 +7,16 @@ class Mettaur{
      * 1 = walk right
      * 2 = jump
      */
-	constructor(game){
+	constructor(game, x, y, gravity){
 		this.game = game;
         this.currentState = 0;
         this.animations = [];
         this.loadAnimation();
+        this.xVelocity = -5;
+        this.yVelocity = 0;
+        this.x = x;
+        this.y = y;
+        this.gravity = gravity;
 		// this.animator = new Animator(ASSET_MANAGER.getAsset("./sprites/mettaur/mettaur-walk.png"),0, 0,32,36,8,0.1);
         // this.animator = new Animator(ASSET_MANAGER.getAsset("./sprites/mettaur/mettaur-jump.png"),0, 0,32,36,7,0.08);
         // this.animator = new Animator(ASSET_MANAGER.getAsset("./sprites/mettaur/mettaur-duck.png"),0, 0,32,36,6,0.5);
@@ -29,45 +27,49 @@ class Mettaur{
 	};
     loadAnimation() {
         this.animations[0] = new Animator(ASSET_MANAGER.getAsset("./sprites/mettaur/mettaur-walk.png"),0, 0,32,36,8,0.1);
-        this.animations[1]= new Animator(ASSET_MANAGER.getAsset("./sprites/mettaur/mettaur-jump.png"),0, 0,32,36,7,0.08);
-        this.animations[2] = new Animator(ASSET_MANAGER.getAsset("./sprites/mettaur/mettaur-duck.png"),0, 0,32,36,6,0.5);
-        this.animations[3] = new Animator(ASSET_MANAGER.getAsset("./sprites/mettaur/mettaur-fall.png"),0, 0,36,38,6,0.5);
+        this.animations[1]= new Animator(ASSET_MANAGER.getAsset("./sprites/mettaur/mettaur-jump.png"),0, 0,32,36,7,0.1);
+        this.animations[2] = new Animator(ASSET_MANAGER.getAsset("./sprites/mettaur/mettaur-duck.png"),0, 0,32,36,7,0.1);
+        this.animations[3] = new Animator(ASSET_MANAGER.getAsset("./sprites/mettaur/mettaur-fall.png"),0, 0,36,38,6,0.1);
         this.animations[4] = new Animator(ASSET_MANAGER.getAsset("./sprites/mettaur/mettaur-fall.png"),0, 0,36,38,6,0.5);
     }
 	update() {
-        //Temporary
-        window.addEventListener("keydown", function (event) {
-            if (event.defaultPrevented) {
-              return; // Do nothing if the event was already processed
-            }   
-        //   console.log(event.key);
-            switch (event.key) {
-              case "q":
-                  this.currentState = 0;
-                // console.log("Q PRessed")
-                break;
-              case "w":
-                this.currentState = 1;
-                break;
-              case "e":
-                this.currentState = 2;
-                break;
-              case "w":
-                this.currentState = 3;
-                break;
-              default:
-                return; // Quit when this doesn't handle the key event.
-            }
-          
-            // Cancel the default action to avoid it being handled twice
-            event.preventDefault();
-          }, true);
+
+      let that = this;
+      //apply gravity to the enemy
+      that.yVelocity += that.gravity;
+      //apply the velocity to the position 
+      that.x += that.xVelocity;
+      that.y += that.yVelocity;
+      window.addEventListener("keydown", function (event) {
+          if (event.defaultPrevented) {
+            return; // Do nothing if the event was already processed
+          }   
+      //   console.log(event.key);
+          switch (event.key) {
+            case "q":
+              that.currentState = 0;
+              break;
+            case "w":
+              that.currentState = 1;
+              break;
+            case "e":
+              that.currentState = 2;
+              break;
+            case "r":
+              that.currentState = 3;
+              break;
+            default:
+              return; // Quit when this doesn't handle the key event.
+          }
+        
+          // Cancel the default action to avoid it being handled twice
+          event.preventDefault();
+        }, true);
 	};
 
 	draw(ctx) {
 		// console.log(this.animator.currentFrame());
-        console.log(this.currentState);
-		this.animations[this.currentState].drawFrame(this.game.clockTick, ctx, 0, 0, 3);
+		this.animations[this.currentState].drawFrame(this.game.clockTick, ctx, this.x, this.y, 2);
 		// console.log(this.game.clockTick);
 		// ctx.drawImage(ASSET_MANAGER.getAsset("./sprites/mettaur/mettaur-walk.png"),0,0);
 	};
