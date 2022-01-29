c8245494389ad2f3539e510f5aa1d73ce9daded3
Artem Potafiy <apotafiy@uw.edu>
2022-01-28 23:59:29 -0800

    adjusted ground enemy

--- main.js
+++ main.js
@@ -40,7 +40,6 @@ ASSET_MANAGER.downloadAll(() => {
     ctx.imageSmoothingEnabled = false;
     let gravity = 0.2;
     //scene manager for scrolling
-    gameEngine.addEntity(new GroundHorse(gameEngine, 256 * 3, 355));
 
     gameEngine.addEntity(new Player(gameEngine, 200, 0, gravity));
     gameEngine.addEntity(new Drill(gameEngine, 11, 2));
@@ -77,9 +76,15 @@ ASSET_MANAGER.downloadAll(() => {
             new Mettaur(gameEngine, 200 + i * 100, 0, gravity)
         );
     }
-    gameEngine.addEntity(new GroundHorse(gameEngine, 36, 1));
-    gameEngine.addEntity(new GroundHorse(gameEngine, 10, 6));
-    gameEngine.addEntity(new GroundHorse(gameEngine, 36, 1));
+    gameEngine.addEntity(new GroundHorse(gameEngine, 36, 0));
+    gameEngine.addEntity(new GroundHorse(gameEngine, 9, 5));
+    gameEngine.addEntity(new GroundHorse(gameEngine, 10, 5));
+    gameEngine.addEntity(new GroundHorse(gameEngine, 11, 5));
+    gameEngine.addEntity(new GroundHorse(gameEngine, 12, 5));
+    gameEngine.addEntity(new GroundHorse(gameEngine, 13, 5));
+    gameEngine.addEntity(new GroundHorse(gameEngine, 14, 5));
+
+    gameEngine.addEntity(new GroundHorse(gameEngine, 36, 0));
     gameEngine.addEntity(new Water(gameEngine));
 
     gameEngine.addEntity(new Ground(gameEngine, 1, 0, 0, 1, 8, 1, 0, 1));
