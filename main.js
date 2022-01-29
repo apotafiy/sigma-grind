09bfd50800b77851831209527217b54403f3d87a
Artem Potafiy <apotafiy@uw.edu>
2022-01-29 13:08:04 -0800

    added more drills

--- main.js
+++ main.js
@@ -43,6 +43,9 @@ ASSET_MANAGER.downloadAll(() => {
 
     gameEngine.addEntity(new Player(gameEngine, 200, 0, gravity));
     gameEngine.addEntity(new Drill(gameEngine, 11, 2, 5));
+    gameEngine.addEntity(new Drill(gameEngine, 14, 2, 5));
+    gameEngine.addEntity(new Drill(gameEngine, 17, 2, 5));
+
     gameEngine.addEntity(new SceneManager(gameEngine));
     gameEngine.addEntity(new Ground(gameEngine, 1, 0, 8, 15, 4, 1));
     gameEngine.addEntity(new Ground(gameEngine, 1, 5, 8, 45, 8, 1, 0, 0));
