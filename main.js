0863cc0273fd741edb3401ba14255d71810a1c26
Lam Mai <lam7.2521@gmail.com>
2022-01-28 22:33:03 -0800

    Fixed merge conflict issues

--- main.js
+++ main.js
@@ -24,7 +24,6 @@ ASSET_MANAGER.queueDownload('./sprites/player/player-idle-43x48.png');
 ASSET_MANAGER.queueDownload('./sprites/player/player-run-51x49.png');
 ASSET_MANAGER.queueDownload('./sprites/player/player-jump-47x80.png');
 ASSET_MANAGER.queueDownload('./sprites/player/player-wallhang-36x65.png');
-ASSET_MANAGER.queueDownload('./sprites/player/player-dash-97x52.png');
 ASSET_MANAGER.queueDownload('./sprites/player/zerox4sheet.png');
 //player attack Animations
 ASSET_MANAGER.queueDownload(
@@ -40,19 +39,19 @@ ASSET_MANAGER.downloadAll(() => {
     ctx.imageSmoothingEnabled = false;
     let gravity = 0.2;
     //scene manager for scrolling
-    gameEngine.addEntity(new Player(gameEngine, 200, 0, gravity));
-    gameEngine.addEntity(new SceneManager(gameEngine));
     gameEngine.addEntity(new GroundHorse(gameEngine, 256 * 3, 355));
 
+    gameEngine.addEntity(new Player(gameEngine, 200, 0, gravity));
+    gameEngine.addEntity(new SceneManager(gameEngine));
     gameEngine.addEntity(new Ground(gameEngine, 1, 0, 8, 15, 4, 1));
     gameEngine.addEntity(new Ground(gameEngine, 1, 5, 8, 45, 8, 1, 0, 0));
     gameEngine.addEntity(new Ground(gameEngine, 1, 50, 2, 10, 16, 1, 0, 1));
 
     gameEngine.addEntity(new Drill(gameEngine, 14, 2));
 
-    gameEngine.addEntity(new Ground(gameEngine, 1, 0, 8, 15, 4, 1));
-    gameEngine.addEntity(new Ground(gameEngine, 1, 5, 8, 45, 8, 1, 0, 0));
-    gameEngine.addEntity(new Ground(gameEngine, 1, 50, 2, 10, 16, 1, 0, 1));
+    // gameEngine.addEntity(new Ground(gameEngine, 1, 1, 7, 8, 1,0,0,1));
+    //Changed 4-> 1 xoffset
+    gameEngine.addEntity(new Ground(gameEngine, 1, 1, 7, 15, 1, 1, 0, 1));
 
     gameEngine.addEntity(new Ground(gameEngine, 1, 1, 6, 8, 1, 1, 0, 1));
     gameEngine.addEntity(new Ground(gameEngine, 1, 3, 5, 2, 1, 1, 0, 0));
@@ -73,18 +72,14 @@ ASSET_MANAGER.downloadAll(() => {
         if (entity instanceof Ground) entity.checkForGrass();
     });
     gameEngine.addEntity(new Mettaur(gameEngine, 800, 0, gravity));
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < 100; i++) {
         gameEngine.addEntity(
-            new Mettaur(gameEngine, 200 + i * 400, 0, gravity)
+            new Mettaur(gameEngine, 200 + i * 100, 0, gravity)
         );
     }
-    gameEngine.addEntity(new GroundHorse(gameEngine, 35, 0));
-    gameEngine.addEntity(new GroundHorse(gameEngine, 9, 5));
-    gameEngine.addEntity(new GroundHorse(gameEngine, 10, 5));
-    gameEngine.addEntity(new GroundHorse(gameEngine, 11, 5));
-    gameEngine.addEntity(new GroundHorse(gameEngine, 12, 5));
-
-    gameEngine.addEntity(new GroundHorse(gameEngine, 35, 0));
+    gameEngine.addEntity(new GroundHorse(gameEngine, 36, 1));
+    gameEngine.addEntity(new GroundHorse(gameEngine, 10, 6));
+    gameEngine.addEntity(new GroundHorse(gameEngine, 36, 1));
     gameEngine.addEntity(new Water(gameEngine));
 
     gameEngine.addEntity(new Ground(gameEngine, 1, 0, 0, 1, 8, 1, 0, 1));
