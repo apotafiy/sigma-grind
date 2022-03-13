12c7321722c3f4a3b640b05c22113da48bbb3cac
Artem Potafiy <apotafiy@uw.edu>
2022-03-12 18:11:48 -0800

    more optimization for time indicator

--- entities/support/TimeIndicator.js
+++ entities/support/TimeIndicator.js
@@ -17,19 +17,21 @@ class TimeIndicator {
             return;
         }
         this.lifespan -= 1500 * this.game.clockTick;
-        this.opacity = Math.max(0, this.lifespan / 1000);
     }
 
     draw(ctx) {
         const text = '-' + Math.floor(this.ms / 1000);
         ctx.font = '20px "Zen Dots"';
         ctx.textAlign = 'left';
+        const fill = ctx.fillStyle;
+        const stroke = ctx.strokeStyle;
+        let color = `rgba(${this.rgb.r},${this.rgb.g},${this.rgb.b}, 1)`;
+        this.opacity = Math.max(0, this.lifespan / 1000);
         if (this.lifespan < this.fadeInflection) {
-            ctx.fillStyle = `rgba(${this.rgb.r},${this.rgb.g},${this.rgb.b}, ${this.opacity})`;
-        } else {
-            ctx.fillStyle = `rgba(${this.rgb.r},${this.rgb.g},${this.rgb.b}, 1)`;
+            color = `rgba(${this.rgb.r},${this.rgb.g},${this.rgb.b}, ${this.opacity})`;
         }
-        ctx.strokeStyle = 'Light blue';
+        ctx.fillStyle = color;
+        ctx.strokeStyle = color;
         ctx.lineWidth = 2;
         ctx.strokeText(
             text,
@@ -42,6 +44,8 @@ class TimeIndicator {
             this.y - this.game.camera.y - (1000 - this.lifespan) * 0.2
         );
         ctx.lineWidth = 1;
+        ctx.fillStyle = fill;
+        ctx.strokeStyle = stroke;
     }
 }
 //ligma balls
